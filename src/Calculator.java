cdeaa6bcb5f475d4672a687dd1cd1d0dcbc7dffe
nixondebantu <nixondebantu@gmail.com>
2023-06-07 07:25:54 +0600

    add font & textfield

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 public class Calculator {
     JFrame frame;
     JPanel panel;
+    JTextField textField;
+    Font calFont = new Font("calibri", Font.BOLD, 25);
 
     Calculator(){
         frame = new JFrame("Swing Calculator");
@@ -16,7 +18,14 @@ public class Calculator {
         panel.setLayout(new GridLayout(4, 4, 10, 10));
         panel.setBackground(new Color(44, 51, 51));
 
+        textField = new JTextField();
+        textField.setBounds(50, 25, 310, 50);
+        textField.setEditable(false);
+        textField.setFont(calFont);
+        textField.setBackground(new Color(203, 228, 222));
+
         frame.add(panel);
+        frame.add(textField);
         frame.setVisible(true);
     }
 }
