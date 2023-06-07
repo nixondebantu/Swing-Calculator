74f95ef290ed554904e48a2b99b49b0c79d5af67
nixondebantu <nixondebantu@gmail.com>
2023-06-07 07:18:29 +0600

    add panel

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 
 public class Calculator {
     JFrame frame;
+    JPanel panel;
 
     Calculator(){
         frame = new JFrame("Swing Calculator");
@@ -10,6 +11,12 @@ public class Calculator {
         frame.getContentPane().setBackground(new Color(44, 51, 51));
         frame.setLayout(null);
 
+        panel = new JPanel();
+        panel.setBounds(50, 100, 310, 300);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+        panel.setBackground(new Color(44, 51, 51));
+
+        frame.add(panel);
         frame.setVisible(true);
     }
 }
