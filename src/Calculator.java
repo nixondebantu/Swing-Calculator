ca662bf3d3712949fb79bb18754d4b3ed850686c
nixondebantu <nixondebantu@gmail.com>
2023-06-07 07:47:27 +0600

    init fubtn

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,6 +7,7 @@ public class Calculator {
     JTextField textField;
     Font calFont = new Font("calibri", Font.BOLD, 25);
     JButton[] numbtn = new JButton[10];
+    JButton[] fubtn = new JButton[9];
 
     Calculator(){
         frame = new JFrame("Swing Calculator");
@@ -16,7 +17,7 @@ public class Calculator {
 
         panel = new JPanel();
         panel.setBounds(50, 100, 310, 300);
-        panel.setLayout(new GridLayout(4, 4, 10, 10));
+        panel.setLayout(new GridLayout(5, 4, 10, 10));
         panel.setBackground(new Color(44, 51, 51));
 
         textField = new JTextField();
@@ -30,6 +31,22 @@ public class Calculator {
             numbtn[i].setFont(calFont);
             numbtn[i].setFocusable(false);
             numbtn[i].setBackground(new Color(14, 131, 136));
+        }
+
+        fubtn[3] = new JButton("/");    //divbtn
+        fubtn[2] = new JButton("X");    //mulbtn
+        fubtn[1] = new JButton("-");    //subbtn
+        fubtn[0] = new JButton("+");    //addbtn
+        fubtn[4] = new JButton(".");    //dotbtn
+        fubtn[5] = new JButton("=");    //equbtn
+        fubtn[6] = new JButton("(-)");    //minbtn
+        fubtn[7] = new JButton("DEL");    //delbtn
+        fubtn[8] = new JButton("AC");    //clrbtn
+
+        for(int i=0 ; i<9 ; i++){
+            fubtn[i].setFont(calFont);
+            fubtn[i].setFocusable(false);
+            fubtn[i].setBackground(new Color(14, 131, 136));
         }
 
         frame.add(panel);
